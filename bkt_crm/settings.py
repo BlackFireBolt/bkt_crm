97d5f7b3b1d2540308ad98574aff381c01d08c37
BlackFireBolt <boltward@gmail.com>
2020-10-08 17:20:40 +0300

    version 0.5.4

--- bkt_crm/settings.py
+++ bkt_crm/settings.py
@@ -90,23 +90,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'bktcrm_db',
-#       'USER': 'bktcrm_admin',
-#        'PASSWORD': 'ji1saVlU',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bktcrm_db',
+        'USER': 'bktcrm_admin',
+        'PASSWORD': 'ji1saVlU',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
