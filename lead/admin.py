1ca35f7a7877a9a9be65dea51fac8e24226cb1b1
BlackFireBolt <boltward@gmail.com>
2020-10-08 17:35:24 +0300

    version 0.5.5

--- lead/admin.py
+++ lead/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Lead, Note
 
-admin.site.register(Lead)
+
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'status', 'created_date', 'manager')
+
+
+admin.site.register(Lead, LeadAdmin)
 admin.site.register(Note)
 
 admin.site.site_header = 'BKT'
