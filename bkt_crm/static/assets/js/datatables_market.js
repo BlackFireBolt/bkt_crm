2a5934f91c8132c83470c89729970663fd766841
BlackFireBolt <boltward@gmail.com>
2020-10-27 12:16:32 +0300

    version 0.8.4

---
+++ bkt_crm/static/assets/js/datatables_market.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+var dt_table = $('.datatable').dataTable({
+        language: dt_language,  // global variable defined in html
+        lengthMenu: [[25, 50, 100, 200], [25, 50, 100, 200]],
+        order: [[ 0, "desc" ]],
+        searching: true,
+        columnDefs: [
+            {
+                data: 'id',
+                targets: [0],
+                orderable: true,
+            },
+            {
+                data: 'name',
+                targets: [1],
+                orderable: true,
+            },
+            {
+                data: 'phone',
+                targets: [2],
+                orderable: true,
+            },
+            {
+                data: 'country',
+                targets: [3],
+                orderable: true,
+            },
+            {
+                data: 'created_date',
+                targets: [4],
+                orderable: true,
+            },
+        ],
+        processing: true,
+        serverSide: true,
+        stateSave: true,
+        ajax: MARKET_LIST_JSON_URL
+    });
+    });
